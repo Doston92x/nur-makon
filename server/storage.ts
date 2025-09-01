b265c0a2c4f0e8975553a8bfebaa8f86a9a47451
Doston92x <92556861+Doston92x@users.noreply.github.com>
2025-09-01 20:16:56 +0500

    Update storage.ts
    rasmlar qoshildi

--- server/storage.ts
+++ server/storage.ts
@@ -135,7 +135,7 @@ export class MemStorage implements IStorage {
         size: "20 sq meter",
         view: "Garden View",
         amenities: ["Free Wi-Fi", "Air Conditioning", "Mini Fridge", "Work Desk"],
-        imageUrl: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600",
+        imageUrl: "./src/assets/standart.jpeg",
         available: true,
       },
       {
@@ -147,7 +147,7 @@ export class MemStorage implements IStorage {
         size: "24 sq meter",
         view: "Gardeb View",
         amenities: ["Free Wi-Fi", "Air Conditioning", "Mini Fridge", "Work Desk"],
-        imageUrl: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600",
+        imageUrl: "./src/assets/superior.jpeg",
         available: true,
       },
       {
@@ -159,7 +159,7 @@ export class MemStorage implements IStorage {
         size: "26 sq meter",
         view: "Garden View",
         amenities: ["Free Wi-Fi", "Balcony", "Air Conditioning", "Mini Fridge"],
-        imageUrl: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600",
+        imageUrl: "./src/assets/305.jpeg",
         available: true,
       },
       {
@@ -171,7 +171,7 @@ export class MemStorage implements IStorage {
         size: "30 sq meter",
         view: "Garden View",
         amenities: ["Free Wi-Fi", "Air COnditioning", "Mini Fridge", "Working Desk", "Smart TV"],
-        imageUrl: "https://images.unsplash.com/photo-1590490360182-c33d57733427?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600",
+        imageUrl: "./src/assets/201.jpeg",
         available: true,
       },
       {
