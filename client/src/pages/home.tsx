d460234f930c044bae6237603576944bc4164355
Doston92x <92556861+Doston92x@users.noreply.github.com>
2025-09-01 20:24:21 +0500

    Update home.tsx
    updated

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&h=1080')",
+            backgroundImage: "./src/assets/ext.jpeg",
           }}
         />
         <div className="absolute inset-0 bg-black bg-opacity-40" />
